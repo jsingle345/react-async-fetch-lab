4420e25d2a8dda7ea64c2f1e8119dab04d378c55
jsingle345 <jsingle345@gmail.com>
2020-11-23 09:42:30 -0600

    commit tree 4d9d33f98c0aed2cced6f1770760683f9653de42

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            spacePeople: []
+        }
+    }
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(data => {
+            this.setState({
+                spacePeople: data
+            })
+        })
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
